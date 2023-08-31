695b4c9ce125d181c0aeafe8796419c6b96d03ab
Hyunmo <hyunmo0417@gmail.com>
2023-08-31 18:50:28 +0900

    feat: header listener

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ var windowWidth = window.matchMedia("screen and (min-width: 1024px)");
 if (windowWidth.matches) {
   //desktop size
   const navi = document.getElementsByClassName("navigation");
+  navi[0].addEventListener("click", (e) => {
+    if (e.target.tagName === "SPAN") {
+      for (const item of navi[0].children) {
+        item.classList.remove("selected");
+      }
+      e.target.classList.add("selected");
+    }
+  });
 }
